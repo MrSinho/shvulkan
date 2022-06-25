acdae10468c03a2a2c1f3961cc15411165c4b358
Anas Boustani <sino3791@gmail.com>
2022-06-25 22:43:13 +0200

    Update shVkDrawLoop.h

--- shvulkan/include/shvulkan/shVkDrawLoop.h
+++ shvulkan/include/shvulkan/shVkDrawLoop.h
@@ -23,11 +23,18 @@ extern void shFrameReset(ShVkCore* p_core, uint32_t thread_idx);
 
 extern void shFrameBegin(ShVkCore* p_core, const uint32_t thread_idx, uint32_t* p_swapchain_image_idx);
 
+#define shDraw(graphics_cmd_buffer, vertex_count_div_stride)\
+	vkCmdDraw(graphics_cmd_buffer, vertex_count_div_stride, 1, 0, 0)
+
 #define shDrawIndexed(graphics_cmd_buffer, index_count)\
 	vkCmdDrawIndexed(graphics_cmd_buffer, index_count, 1, 0, 0, 0)
 
-#define shDraw(graphics_cmd_buffer, vertex_count_div_stride)\
-	vkCmdDraw(graphics_cmd_buffer, vertex_count_div_stride, 1, 0, 0)
+#define shDrawInstances(graphics_cmd_buffer, vertex_count_div_stride, instance_count)\
+	vkCmdDraw(graphics_cmd_buffer, vertex_count_div_stride, instance_count, 0, 0)
+
+#define shDrawIndexedInstances(graphics_cmd_buffer, index_count, instance_count)\
+	vkCmdDrawIndexed(graphics_cmd_buffer, index_count, instance_count, 0, 0, 0)
+
 
 extern void shFrameEnd(ShVkCore* p_core, const uint32_t thread_idx, const uint32_t swapchain_image_idx);
 
