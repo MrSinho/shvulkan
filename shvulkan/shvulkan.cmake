cd116d54a57101d602bf159e4509a14a0e256734
Anas Boustani <sino3791@gmail.com>
2024-09-26 15:48:59 +0200

    Update shvulkan.cmake

--- shvulkan/shvulkan.cmake
+++ shvulkan/shvulkan.cmake
@@ -16,15 +16,13 @@ message(STATUS "shvulkan message: found Vulkan")
 message(STATUS "shvulkan message: Vulkan_INCLUDE_DIR: ${Vulkan_INCLUDE_DIR}")
 message(STATUS "shvulkan message: Vulkan_LIBRARY:     ${Vulkan_LIBRARY}")
 
-set(SH_VULKAN_SRC_DIR shvulkan/src/shvulkan)
-
 add_library(shvulkan STATIC 
-    ${SH_VULKAN_SRC_DIR}/shVulkan.c
+    ${SH_VULKAN_ROOT_DIR}/shvulkan/src/shvulkan/shVulkan.c
 )
 
 target_include_directories(shvulkan PUBLIC 
     ${Vulkan_INCLUDE_DIR}
-    ${CMAKE_CURRENT_SOURCE_DIR}/shvulkan/include
+    ${SH_VULKAN_ROOT_DIR}/shvulkan/include
 )
 
 target_link_libraries(shvulkan PUBLIC ${Vulkan_LIBRARY})
