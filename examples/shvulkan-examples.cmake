273d7f214af03c2f2dad322b4f9ff47193cf1697
Anas Boustani <sino3791@gmail.com>
2024-09-26 23:18:46 +0200

    Update shvulkan-examples.cmake

--- examples/shvulkan-examples.cmake
+++ examples/shvulkan-examples.cmake
@@ -39,8 +39,9 @@ if (WIN32)
 target_link_libraries(shvulkan-clear-color PUBLIC shvulkan glfw)
 target_link_libraries(shvulkan-scene       PUBLIC shvulkan glfw)
 elseif (UNIX)
-target_link_libraries(shvulkan-clear-color PUBLIC shvulkan glfw X11)
-target_link_libraries(shvulkan-scene       PUBLIC shvulkan glfw X11)
+target_link_libraries(shvulkan-clear-color    PUBLIC shvulkan glfw X11 libm)
+target_link_libraries(shvulkan-scene          PUBLIC shvulkan glfw X11 libm)
+target_link_libraries(shvulkan-headless-scene libm)
 endif(WIN32)
 
 set_target_properties(
